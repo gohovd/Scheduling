bf0d0bc2132fcb52dc771260ffc1cde50645bb8f
gohovd <goran.hovde@gmail.com>
2015-04-08 19:41:33 +0200

    OKIDOK

---
+++ src/DiskScheduler.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Gøran on 08.04.2015.
+ */
+public interface DiskScheduler {
+    //service the requests
+    //return the amount of head movement
+    //for the particular algorithm
+    public int serviceRequests(); //Returns the amount of head movement.
+}
---
+++ src/FCFS.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Gøran on 08.04.2015.
+ */
+public class FCFS implements DiskScheduler {
+
+    /**
+     * The first-come first-server scheduling algorithm.
+     */
+    public FCFS(){
+
+    }
+
+    public int serviceRequests(){
+        return 0;
+    }
+}
