203cc68b8a4f7dcc626844e54c0454b787b581fe
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2015-04-08 20:13:09 +0200

    SSTF rekt

--- src/SSTF.java
+++ src/SSTF.java
@@ -1,5 +1,60 @@
-/**
- * Created by Per-Olav on 08.04.2015.
- */
-public class SSTF {
+class SSTF implements DiskScheduler{
+    int[] queue;
+    int initialCylinder;
+
+    public SSTF(int[] queue, int initialCylinder){
+        this.queue = queue;
+        this.initialCylinder = initialCylinder;
+    }
+
+    public int serviceRequests(){
+        int headMovement = 0;
+        int prev = initialCylinder;
+        int [] rpath = path();
+        for (int i=0; i < rpath.length; i++) {
+            headMovement += Math.abs(rpath[i]-prev);
+            prev = rpath[i];
+        }
+        return headMovement;
+    }
+
+    public int[] path(){
+        int [] resultPath = new int[queue.length];
+        int now = initialCylinder;
+        int [] requests = new int[queue.length];
+        for (int i = 0; i < queue.length; i++){
+            requests[i] = queue[i];
+        }
+        for (int i = 0; i < resultPath.length; i++){
+            int closest = closest(now, requests);
+            resultPath[i] = closest;
+            now = closest;
+        }
+        return resultPath;
+    }
+
+    int closest(int k, int[] requests){
+        int min = 5000000;
+        int minPos = -1;
+        for (int i = 0; i < requests.length; i++){
+            if (requests[i] == -1) continue;
+            else if  (Math.abs(k-queue[i]) < min) {
+                minPos = i;
+                min = Math.abs(k-queue[i++]);
+            }
+        }
+        int nearestCylinder = requests[minPos];
+        requests[minPos] = -1;
+        return nearestCylinder;
+    }
+
+    public void println(){
+        System.out.println("SSTF head movement = " + serviceRequests());
+
+        System.out.print("SSTF Path = ");
+        for(int i: path()){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
 }
