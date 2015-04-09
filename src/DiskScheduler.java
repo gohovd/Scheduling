9f5689c2013eef97a64c53ee1a09e65491633985
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2015-04-09 14:17:07 +0200

    Fjerna author slik ingen får en flau opplevelse på innlevering

--- src/DiskScheduler.java
+++ src/DiskScheduler.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
 
-/**
- * Created by Gøran on 08.04.2015.
- */
+
 public interface DiskScheduler {
     public int serviceRequests(); //Returns the amount of head movement.
 }
--- src/Runnable.java
+++ src/Runnable.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Created by Gøran on 08.04.2015.
- */
 public class Runnable {
 
     static FCFS fcfs;
