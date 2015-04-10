9351b24cc27630bad3d1330d9ec4bf075cdb40e8
gohovd <goran.hovde@gmail.com>
2015-04-10 17:39:33 +0200

    Div refactoring, endra tekst og shit.

--- src/Runnable.java
+++ src/Runnable.java
@@ -8,7 +8,7 @@ public class Runnable {
     static SSTF sstf;
     static SCAN scan;
     static LOOK look;
-    static final int initialCylinder = 50; //starting position(?)
+    static int headstart = 0; //starting position(?)
     static int[] ref;
 
 
@@ -19,27 +19,29 @@ public class Runnable {
         while (!ack) {
             System.out.println("Please follow the instructions in the console. \n" +
                     "Text providing instructions will have a percentage symbol prefix. \n");
-            System.out.println("% Enter a reference string containing the pages for the simulation. \n" +
-                    "% Example reference (corr. syntax): 50 10 39 42 11 24 30. \n" +
+            System.out.println("% Enter a reference string for the simulation. \n" +
+                    "% Example reference (corr. syntax): 50,10,39,42,11,24,30. \n" +
                     "% The headspace for FCFS, SSTF, SCAN and LOOK will be returned. \n" +
-                    "% Along with the sequence of operations for each scheduling algorithm.");
+                    "% Along with the sequence of operations almost every scheduling algorithm.");
             x = userInput.nextLine();
             if (x.isEmpty()) {
                 System.out.println("% The input string cannot be empty.");
             } else if (!x.isEmpty()) {
                 int no = 0;
-                String[] split = x.split(" ");
+                String[] split = x.split(",");
                 ref = new int[split.length];
                 for (String s : split) {
                     int sint = Integer.parseInt(s);
                     ref[no] = sint;
+                    headstart = ref[0];
                     no++;
                 }
                 ack = true;
             }
         }
+        System.out.println("IC: " + headstart);
         //here we pass the array of integers to the various scheduling algorithms..
-        sstf = new SSTF(ref, initialCylinder);
+        sstf = new SSTF(ref, headstart);
         System.out.println("SSTF headspace: " + sstf.serviceRequests());
         System.out.print("SSTF path: ");
         for(int i : sstf.returnPath()){
@@ -48,7 +50,7 @@ public class Runnable {
         //change line..
         System.out.println("\n");
 
-        fcfs = new FCFS(ref, initialCylinder);
+        fcfs = new FCFS(ref, headstart);
         System.out.println("FCFS headspace: " + fcfs.serviceRequests());
         System.out.print("FCFS path: ");
         for(int i : fcfs.returnPath()) {
@@ -64,12 +66,12 @@ public class Runnable {
                 max = i;
             }
         }
-        scan = new SCAN(ref, initialCylinder, 1000);
+        scan = new SCAN(ref, headstart, 1000);
         System.out.println("SCAN headspace: " + scan.serviceRequests());
 
         //System.out.println("\n");
 
-        look = new LOOK(ref, initialCylinder);
+        look = new LOOK(ref, headstart);
         System.out.println("LOOK headspace: " + look.serviceRequests());
     }
 }
